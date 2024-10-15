e0fa9f905595c490aed2ac8c22057e62c22d6c2c
Parizetm <151616242+ParizetM@users.noreply.github.com>
2024-10-15 16:30:54 +0200

    c bo

--- code/game.php
+++ code/game.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +9,7 @@
     <script src="js/script.js"></script>
     <title>Partie en cours - Démineur</title>
 </head>
+
 <body class="bg-blur h-screen flex flex-col justify-between items-center">
 
     <!-- Titre -->
@@ -17,27 +19,27 @@
     <div class="flex justify-center items-center my-8">
         <table class="table-auto border-collapse bg-gray-700">
             <?php
-            $difficulty = "easy";
+            $difficulty = "hardcore";
 
-            if ($difficulty == "easy"){
+            if ($difficulty == "easy") {
                 $row = 9;
                 $col = 9;
                 $mines = 10;
             }
 
-            if ($difficulty == "medium"){
+            if ($difficulty == "medium") {
                 $row = 16;
                 $col = 16;
                 $mines = 40;
             }
 
-            if ($difficulty == "hard"){
+            if ($difficulty == "hard") {
                 $row = 16;
                 $col = 30;
                 $mines = 99;
             }
 
-            if ($difficulty == "hardcore"){
+            if ($difficulty == "hardcore") {
                 $row = 24;
                 $col = 30;
                 $mines = 195;
@@ -56,8 +58,18 @@
             for ($r = 0; $r < $row; $r++) {
                 echo "<tr>";
                 for ($c = 0; $c < $col; $c++) {
-                    echo '<td class="border border-gray-500 w-7 h-4 text-center text-white">';
-                    echo '<button id="' . $r . ';' . $c . '" class="case-solid w-full h-full" onclick="CaseBreak(' . $r . ',' . $c . ')"></button>';
+                    $randomNumber = mt_rand(1, 100);
+                    if ($randomNumber <= 60) {
+                        $skin = "block-1";
+                    } else if ($randomNumber > 60 && $randomNumber <= 73.333) {
+                        $skin = "block-2";
+                    } else if ($randomNumber > 73.333 && $randomNumber <= 86.666) {
+                        $skin = "block-3";
+                    } else if ($randomNumber > 86.666 && $randomNumber <= 100) {
+                        $skin = "block-4";
+                    }
+                    echo '<td class="  w-7 h-4 text-center text-white">';
+                    echo '<button id="' . $r . ';' . $c . '" class="' . $skin . ' case-solid  w-full h-full" onclick="CaseBreak(' . $r . ',' . $c . ')"></button>';
                     echo "</td>";
                 }
                 echo "</tr>";
@@ -74,4 +86,5 @@
     </div>
 
 </body>
+
 </html>
