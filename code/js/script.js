19ffbe92b4ba98f1594e41cf9403bf2c9c7d68dc
Félis Maillard <151616377+FelisMaillard@users.noreply.github.com>
2024-10-15 16:57:24 +0200

    debut init

--- code/js/script.js
+++ code/js/script.js
@@ -1,3 +1,12 @@
+var row, col, mines;
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    row = document.getElementById("row").textContent.trim();
+    col = document.getElementById("col").textContent.trim();
+    mines = document.getElementById("mines").textContent.trim();
+});
+
 function CaseBreak(r, c) {   
     var caseClicked = document.getElementById(r + ";" + c);
     caseClicked.classList.replace("case-solid", "case-break"); 
@@ -7,5 +16,9 @@ function CaseBreak(r, c) {
 }
 
 function Initialise(r, c) {
-    
+    for (var i = 0; i < mines; i++) {
+        var mineRow = Math.floor(Math.random() * row);
+        var mineCol = Math.floor(Math.random() * col);
+        var mineId = mineRow + ";" + mineCol;
+    }
 }
